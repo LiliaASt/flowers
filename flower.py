6aff23a879c80fef09b9e430f6f3a716e00210f3
LiliaASt <lastsatryan2704@mail.ru>
2023-12-21 22:53:17 +0300

    Getting links to images

--- flower.py
+++ flower.py
@@ -1,6 +1,11 @@
 import os
 import requests
 
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 def creating_folder(name: str) -> None:
     if not os.path.isdir(name):
@@ -9,3 +14,26 @@ def creating_folder(name: str) -> None:
     else:
         print("The folder already exists")
 
+def getting_links(query:str, quantity:int) -> None:
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    time.sleep(2)
+    url = f"https://yandex.ru/images/search?text={query}"
+    driver.get(url=url)
+    time.sleep(2)
+    driver.maximize_window()
+    time.sleep(8)
+    view_all_button = driver.find_element(By.CSS_SELECTOR, "a.Link.SimpleImage-Cover")
+    view_all_button.click()
+
+    with open(f"{query}.txt", "w") as file:
+        for link in range(quantity):
+            try:
+                time.sleep(1)
+                link = driver.find_element(By.CSS_SELECTOR, "a.Button2_view_action").get_attribute("href")
+                file.write(link + "\n")
+                driver.find_element(By.CSS_SELECTOR, "div.CircleButton:nth-child(4)").click()
+            except:
+                continue
+    driver.close()
+    driver.quit()
+    print("The files are ready")
